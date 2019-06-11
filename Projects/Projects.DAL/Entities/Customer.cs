ee8ef73bc5699d391c375ae9c4819c12ae86440f
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-11 11:51:04 +0600

    init Entities

---
+++ Projects/Projects.DAL/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projects.DAL.Entities
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Fax { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
---
+++ Projects/Projects.DAL/Entities/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projects.DAL.Entities
+{
+    [Table("Project")]
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime DateStart { get; set; }
+        public Nullable<DateTime> DateEnd { get; set; }
+        public int Priority { get; set; }
+        public string Comment { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
