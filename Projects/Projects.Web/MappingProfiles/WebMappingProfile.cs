bcd4cb170d4678061aa349b9b72cf8b1daa7e5f5
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-11 15:52:13 +0600

    WebMappingProfiles

---
+++ Projects/Projects.Web/MappingProfiles/WebMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Projects.BLL.DTO;
+using Projects.Web.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projects.Web.MappingProfiles
+{
+    public class WebMappingProfile : Profile
+    {
+        public WebMappingProfile()
+        {
+            CreateMap<ExecutorCompanyDTO, ExecutorCompanyVM>(MemberList.None).ReverseMap();
+            CreateMap<PositionDTO, PositionVM>(MemberList.None).ReverseMap();
+            CreateMap<EmployeeDTO, EmployeeVM>(MemberList.None).ReverseMap();
+            CreateMap<CustomerDTO, CustomerVM>(MemberList.None).ReverseMap();
+            CreateMap<ProjectDTO, ProjectVM>(MemberList.None).ReverseMap();
+        }
+    }
+}
