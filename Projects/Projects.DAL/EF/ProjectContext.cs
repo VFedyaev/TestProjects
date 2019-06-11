eecdac0ec7f9d382b84efabf535aefe73cec49aa
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-11 12:09:09 +0600

    init Interfaces

--- Projects/Projects.DAL/EF/ProjectContext.cs
+++ Projects/Projects.DAL/EF/ProjectContext.cs
@@ -1,10 +1,5 @@
 ﻿using Projects.DAL.Entities;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Projects.DAL.EF
 {
---
+++ Projects/Projects.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Projects.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<ExecutorCompany> ExecutorCompanies { get; set; }
+        IRepository<Position> Positions { get; set; }
+        IRepository<Employee> Employees { get; set; }
+        IRepository<Customer> Customers { get; set; }
+        IRepository<Project> Projects { get; set; }
+        IRepository<ProjectEmployee> ProjectEmployees { get; set; }
+
+        void Save();
+    }
+}
