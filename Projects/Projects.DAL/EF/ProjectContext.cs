98540318be9688af2b24d8307b4abd1de50247cf
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-11 12:00:11 +0600

    init EF

---
+++ Projects/Projects.DAL/EF/ProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using Projects.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.DAL.EF
+{
+    public class ProjectContext : DbContext
+    {
+        DbSet<ExecutorCompany> ExecutorCompanies { get; set; }
+        DbSet<Position> Positions { get; set; }
+        DbSet<Employee> Employees { get; set; }
+        DbSet<Customer> Customers { get; set; }
+        DbSet<Project> Projects { get; set; }
+        DbSet<ProjectEmployee> ProjectEmployees { get; set; }
+
+        public ProjectContext(string connectionString) : base(connectionString)
+        {
+            Database.SetInitializer<ProjectContext>(null);
+        }
+    }
+}
