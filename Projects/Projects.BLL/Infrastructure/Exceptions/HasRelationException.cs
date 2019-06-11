e0d072a808c603a23c4da9ea2ebeeb5756f5c749
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-11 13:19:21 +0600

    init infrastructure, mappingProfiles

---
+++ Projects/Projects.BLL/Infrastructure/Exceptions/HasRelationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Projects.BLL.Infrastructure.Exceptions
+{
+    public class HasRelationsException : Exception
+    {
+        static string message = "Item has relations.";
+
+        public HasRelationsException() : base(message) { }
+    }
+}
---
+++ Projects/Projects.BLL/MappingProfiles/BLLMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Projects.BLL.DTO;
+using Projects.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.BLL.MappingProfiles
+{
+    public class BLLMappingProfile : Profile
+    {
+        public BLLMappingProfile()
+        {
+            CreateMap<ExecutorCompany, ExecutorCompanyDTO>(MemberList.None).ReverseMap();
+            CreateMap<Position, PositionDTO>(MemberList.None).ReverseMap();
+            CreateMap<Employee, EmployeeDTO>(MemberList.None).ReverseMap();
+            CreateMap<Customer, CustomerDTO>(MemberList.None).ReverseMap();
+            CreateMap<Project, ProjectDTO>(MemberList.None).ReverseMap();
+            CreateMap<ProjectEmployee, ProjectEmployeeDTO>(MemberList.None).ReverseMap();
+        }
+    }
+}
